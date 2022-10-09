b4e335ab9d4ddf23b6ba2a4cfd23b1424120ec75
Brian Victor <brian_saka@hotmail.com>
2022-10-09 00:02:27 -0300

    added toggle switch

--- src/components/layout/styles.ts
+++ src/components/layout/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Grid = styled.div `
     display:  grid;
     grid-template-columns: 250px auto;
-    grid-template-rows: 60px auto;
+    grid-template-rows: 75px auto;
     grid-template-areas: 
         'MH MH'
         'AS CT';
