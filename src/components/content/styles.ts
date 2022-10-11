ae1bccb8b6c68df0d31be450b5d2149312f703e9
Brian Victor <brian_saka@hotmail.com>
2022-10-11 18:23:56 -0300

    added scrollbar layout

--- src/components/content/styles.ts
+++ src/components/content/styles.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
+export const Container = styled.div`
     grid-area: CT;
-    color:  ${props => props.theme.colors.white};
+    color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.primary};
-
-    padding: 25px;
+    padding: 25px;  
+    height: calc(100vh - 125px); 
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
+    }
 `;
